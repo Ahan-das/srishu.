9c4ae4546ebf0419ff5391b08b7960cd6c5917bf
Ahan-das <ahandas01@gmail.com>
2025-02-06 00:30:38 +0530

    first commit

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -18,9 +18,15 @@ const Message = () => {
           Bhalo thakis ar parle Apni nijeke please nesha drobbo, ulto palta time e ar ulto palta jinish
           khaoya, self doubt, depression, nijer toxic classmates ar nijeke weird bhaba theke dure thakben.
           Just a friendly advise.
-
-          Ar ekta kotha tui amake bhalobhasis ki na bhabhis amar proti kono feelings thakuk na thakuk 
-          Ami toke sobh somoy bhalobhasbo Pagli😒.
+          Ami bhebechilam amra ager moton kotha bolte suru korle sobh thik hoye jabe abar notun kore somporko
+          ta start korte cheyechilam.Ami kono din e chaini amader somporko ta dujoner jed er jnno erom kore sesh hoye jak 
+          kintu tui to icche more geche bole chole geli , ami toke mananor onek chesta korechi ar parchi na Sorry.
+          Amar o kosto hoy chele holeo amaro kanna paay ki korbo kauke bhalobashle hoyeto erom e hoy Jai hok char tor jonmo din
+          er din abar ulto palta bokchi.
+          Ajke theke toke messege kore, instagram e follow request pathiye, bar bar dekha korte bole birokto 
+          korbo na.
+          Tata.
+          Bhalo thakis ar nijer ektu jotno koris, khub Lyadhkhor tui😄.
       
         </p>
         <p className="text-lg text-pink-600 font-semibold">
