560082c592af947bb24cab35a0255afb712a1c08
Ahan-das <ahandas01@gmail.com>
2025-02-10 21:10:00 +0530

    Update Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -14,25 +14,24 @@ const Message = () => {
           Dear Sristi,
         </h1>
         <p className="text-xl text-gray-700 leading-relaxed mb-6">
-           Happy Birthday Sristi many many happy returns of the day. Khub miss kori re toke.
+Happy Birthday Sristi many many happy returns of the day. Khub miss kori re toke.
           Apni nijeke please nesha drobbo, ulto palta time e ar ulto palta jinish khaoya, self doubt, depression, 
           nijer toxic classmates ar nijeke weird bhaba theke dure thakben.
           Just a friendly advise.
           Ami kono din e chaini amader somporko ta erom kore sesh hoye jak .
-
-          Bhebechilam amra ager moton kotha bolte suru korle sobh thik hoye jabe abar notun kore somporko
-          ta start korte cheyechilam ager baar jei bhul gulo korechilam sei gulo thik korte cheyechilam. 
-          But tui to icche more geche bole chole geli , ami toke mananor onek chesta korechi kintu tor jokhon
-          akdom e ar icche nei aj theke toke messege kore, instagram e follow request pathiye, bar bar 
-          dekha korte bole birokto korbo na.
-          Ar dekh tui amar jnno kono din e option chilis na one and only choice chilis tai tui icche more geche bole 
-          sobh kichu sesh korleo ami parini sobh kichu bhule jete ar parbo boleo mone hoy na. Kono icche hole fere ashis 
-          opekha korbo tor jnno na ashleo kono oshubidha nei.
+Ar ajke ami toke cutoff korar ashol karon ta bolchi dekh tor hoyeto mone nei kintu class 11 e tui onko porikha r ager din amake ekta text koris purota bolchina just briefly bolchi okhane likhechis amader ar kotha hobe na bhalo thakis tarpor amake instagram whatsApp duto tei block kore dis tarpor abar kichu mash pore amake message korechilis okane tui likhechilis tui genshin impact addict hoye gechis tokhon amar kharap lege chilo ei bhebe je tui ato mash genshin impact khelchis kintu ekbar o amake message korar time pash ni ar tokhon o tui kauke id khelte dis ni tui diyechilis class 12 e. Tao ami tokhon oi shob bhule giye tor sathe ager moton kotha bolte suru korechilam toke porashonay help korte cheychilam. Jai hok class 11 ta ki hoyeche oto matter korena kintu tui bolli class 11 r tor mental health thik chilo na ekbar tui amar sathe tor problem gulo share kore dekhte partis kono help na korte parleo ami shuntam seta na kore tui amake cutoff kore dili. Tarpor e class 12 e kichu din thik chilo kintu tarpor e tui ek din du din pore reply kore suru koris tai amio toke late reply korte suru kore dili ar ami eto boro chagol bar bar Instagram gulo check kortam tui msg korechis kina, amar kache Instagram er chat nei Instagram id delete kore diyechi tui thakle dekhenis mitthe kotha bolchi kina . Dekh tor kache career taka poisa important hote pare tui ki sobar kache important kintu amar kache tui sob cheye besi important chilis. Jiggesh korechilis na eto time, effort keno ditam toke , simple reason toke bhalobhastam bole ar kichui na. NDA chere di barite jogra kore tor sathe same college e bhorti hoi tor just because ami tor sathe time spend korte cheychilam toke aro bhalo kore jante cheyechilam. Tor kache ami ki chilam jani na kintu tui amar kono din e option chilis na one and only choice chilis. Amar sotti khub kharap legechilo jokhon tui bari jaoyar jnno baron korli karon amar mone hoye chilo ami jar jnno eto dur elam sei naa kore dilo. Ar kothao na kothao eshe amar mone hoyechilo tor hoyeto amar proti kono feelings e nei. Tai Ekta somoy por eshe ami realise kori hoyechilo je toke birokto kore ar labh nei tokhon ami toke cut off kore di. Tui jokhon January msg korechilis bhebechilam ebar sob kichu thik koreni abar ekbar chesta kore dekhi ager ja hoyeche bhule giye notun kore start kori kintu tui amar jed er jnno future e problem hobe bole icche more geche bole chole geli ami toke mananor o chesta korlam kintu tui bhabli ami toke jor korchi amar sathe relationship e ashar jnno ar tui kon jogote bash koris jani na kintu tui sotti bhebechilis tui msg korle ami birokto hoi? tui weird bole, toke samne dekhe pochondho hoyni bole ami tor sathe kotha boli ni toke cut off kore diyechi? Ha ami jani tor sathe ami jei gulo toke bolechi ogulo bhul toke orom kore bola uchit hoyeni. Jokhon tor sathe sobh cheye besi kotha bolar dorkar chilo tokhoni boli ni.  Jai hok eta amar perspective chilo. Ei gulo likhtam karon amar mone hoyeche tor ei gulo jana uchit.
+ Aj theke toke messege kore, instagram e follow request pathiye, bar bar dekha korte bole birokto korbo na.
           
-          Bhalo thakis ar nijer ektu jotno koris jotno I mean rupchorcha r kotha bolini tui tor moton kore perfect
-          nijer sorir e jotno nis, thik thak time e khas na ghumash o na, khub Lyadhkhor tui😄.
+          Bhalo thakis ar nijer ektu jotno koris jotno I mean rupchorcha r kotha bolini tui tor moton kore perfect nijer sorir e jotno nis, thik thak time e khas na ghumash o na, khub Lyadhkhor tui😄.
           Tata, Happy Birthday.
-      
+
+
+
+
+
+
+
+          
         </p>
         <p className="text-lg text-pink-600 font-semibold">
           From Your's Truely Meow😊
