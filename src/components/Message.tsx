d81c903e98a22d2944096c33445f17743e4825a2
Ahan-das <ahandas01@gmail.com>
2025-02-12 18:45:42 +0530

    Update Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -14,13 +14,13 @@ const Message = () => {
           Dear Sristi,
         </h1>
         <p className="text-xl text-gray-700 leading-relaxed mb-6">
+
 Happy Birthday Sristi many many happy returns of the day. Khub miss kori re toke.
  Apni nijeke please nesha drobbo, ulto palta time e ar ulto palta jinish khaoya, self doubt, depression, nijer toxic classmates ar nijeke weird bhaba theke dure thakben.
           Just a friendly advise.
           Ami kono din e chaini amader somporko ta erom kore sesh hoye jak .
-Tui hoyeto bhebechis tui jaoya niye raag kore chilis bole ami tor upor raag kore toke cut off kore diyechilam kintu ashole e bepar ta seta chilo na cutoff korar reason ta ekhane bolte parbo na karon text ta tale onek boro hoye jabe tui sotti jante chaile bole debo . Ha eta sotti amar reasons chilo tor upor raag korar kintu amar method ta bhul chilo. Ar dekh ami desperate na tor sathe relationship e ashar jnno tui amar jnno kono din e option chilis na one and only choice chilis tai ami bhebechilam toke ar ekbar mananor chesta kori karon ami toke amar life theke harate chaini kintu tui to bhabli ami toke jor korchi amar sathe relationship e ashar jnno.Sudhu eta bolbo amra dujonei kintu arekbar chesta kore dekhtei partam. 
-          Jai hok baad de eishob nijer life e focus kor jeta hoye geche seta to ami bodla te parbo na ar notun kore tui start o korbi na to, aj theke ar message kore, Instagram e follow request pathiye, bar bar dekha korte bole toke birokto korbo na.
-          Bhalo thakis ar nijer ektu jotno koris jotno I mean rupchorcha r kotha bolini tui tor moton kore perfect nijer sorir e jotno nis, thik thak time e khas na ghumash o na, khub Lyadhkhor tui😄.
+Tui hoyeto bhebechis tui jaoya niye raag kore chilis bole ami tor upor jed kore toke cut off kore diyechilam kintu ashole e bepar ta seta chilo na cutoff korar reason ta ekhane bolte parbo na karon text ta tale onek boro hoye jabe pore jiggesh koris bole debo . Ha eta sotti amar method ta bhul chilo kintu amar tor upor raag korar reasons chilo . Ar dekh ami desperate na tor sathe relationship e ashar jnno tui amar jnno kono din e option chilis na one and only choice chilis tai ami toke mananor chesta korechilam karon ami toke amar life theke harate chaina tor upor ami kono din give up korte chai ni kintu tui ki bolli tor icche more geche ami toke jor korchi. Jai hok puruno kotha baad de tui jokhon cholei jete cheychis jaa ar atkabo na toke. Aj theke ar message kore, Instagram e follow request pathiye, bar bar dekha korte bole birokto korbo na. Sudhu etai bolbo tui amay bhul bhujechilis.
+          Bhalo thakis ar nijer ektu jotno koris jotno I mean rupchorcha r kotha bolini toke sobh somoy sundor lage just porer baar saree porle onno juto poris. Nijer sorir e jotno nis tui thik thak time e khas o na ghumash o na, khub Lyadhkhor tui😄.
           Tata, Happy Birthday.
 
 
