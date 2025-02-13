5c3c941d6f00f773b6a5c38177c6c2526a3c5441
Ahan-das <ahandas01@gmail.com>
2025-02-13 20:06:55 +0530

    Update Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -19,10 +19,9 @@ Happy Birthday Sristi many many happy returns of the day. Khub miss kori re toke
  Apni nijeke please nesha drobbo, ulto palta time e ar ulto palta jinish khaoya, self doubt, depression, nijer toxic classmates ar nijeke weird bhaba theke dure thakben.
           Just a friendly advise.
           Ami kono din e chaini amader somporko ta erom kore sesh hoye jak .
-Tui hoyeto bhebechis tui jaoya niye raag kore chilis bole ami tor upor jed kore toke cut off kore diyechilam kintu ashole e bepar ta seta chilo na cutoff korar reason ta ekhane bolte parbo na karon text ta tale onek boro hoye jabe pore jiggesh koris bole debo . Ha eta sotti amar method ta bhul chilo kintu amar tor upor raag korar reasons chilo . Ar dekh ami desperate na tor sathe relationship e ashar jnno tui amar jnno kono din e option chilis na one and only choice chilis tai ami toke mananor chesta korechilam karon ami toke amar life theke harate chaina tor upor ami kono din give up korte chai ni kintu tui ki bolli tor icche more geche ami toke jor korchi. Jai hok puruno kotha baad de tui jokhon cholei jete cheychis jaa ar atkabo na toke. Aj theke ar message kore, Instagram e follow request pathiye, bar bar dekha korte bole birokto korbo na. Sudhu etai bolbo tui amay bhul bhujechilis.
-          Bhalo thakis ar nijer ektu jotno koris jotno I mean rupchorcha r kotha bolini toke sobh somoy sundor lage just porer baar saree porle onno juto poris. Nijer sorir e jotno nis tui thik thak time e khas o na ghumash o na, khub Lyadhkhor tui😄.
-          Tata, Happy Birthday.
-
+Tui hoyeto bhebechis tui jaoya niye raag korechilis bole ami tor upor jed kore toke cut off kore diyechilam kintu ashole e bepar ta seta chilo na pore kono din jiggesh koris bole debo . Ha sotti ami bhul kore felechi . Tui amar jnno kono din e option chilis na one and only choice chilis tai ami toke mananor chesta korechilam karon ami toke amar life theke harate chaina tor upor ami kono din give up korte chai na kintu tui ki bolli tor icche more geche ami toke jor korchi, kintu actually cheyechilam tui amake just akta last chance diye dekhte partis promise korchi regret kortis na. 
+          Bhalo thakis ar nijer ektu jotno koris jotno I mean rupchorcha r kotha bolini toke sobh somoy sundor lage just nijer sorir e jotno nis tui thik thak time e khas o na ghumash o na, khub Lyadhkhor tui😄.
+         Ulto palta jinish dekhis na,Tata.
 
           
         </p>
